668d35388c264e1691270dd4659eb60c582eaeec
CIscoX <13581535186@163.com>
2026-06-10 21:07:46 +0800

    fix : fix some configs

--- sid/sid效果评测/meta_info_join.py
+++
@@ -1,4 +0,0 @@
-# TODO
-"""
-功能 : 用item <-> sid 映射dict 和原始item info做join，并写成json
-"""
--- sid/sid效果评测/使用示例.sh
+++ sid/sid效果评测/使用示例.sh
@@ -1,15 +1,15 @@
-python /mnt/graduation_paper/sid/sid效果评测/case_extract.py --csv data.csv --jsonl meta.jsonl -m 5 -k 3 --output ./result
+python /path --csv data.csv --jsonl meta.jsonl -m 5 -k 3 --output ./result
 
-python /mnt/graduation_paper/sid/sid效果评测/case_extract.py --csv data.csv --jsonl meta.jsonl -m 5 -k 3 --at-least --output ./result
+python /path --csv data.csv --jsonl meta.jsonl -m 5 -k 3 --at-least --output ./result
 
-python /mnt/graduation_paper/sid/sid效果评测/case_extract.py --csv /mnt/data/sid/Beauty/checkpoint_120000/itemid_to_sid.csv --jsonl /mnt/data/Beauty_items.jsonl -m 2 -k 5 --seed 2020 \
-    --output /mnt/graduation_paper/sid/sid效果评测/抽样结果/2个item的
-
-
-
-python /mnt/graduation_paper/sid/sid效果评测/case_extract.py --csv /mnt/data/sid/Beauty/checkpoint_120000/itemid_to_sid.csv --jsonl /mnt/data/Beauty_items.jsonl -m 10 -k 5 --seed 2020 \
-    --output /mnt/graduation_paper/sid/sid效果评测/抽样结果/10个item的
+python /path --csv /mnt/data/sid/Beauty/checkpoint_120000/itemid_to_sid.csv --jsonl /mnt/data/Beauty_items.jsonl -m 2 -k 5 --seed 2020 \
+    --output /path
 
 
-python /mnt/graduation_paper/sid/sid效果评测/case_extract.py --csv /mnt/data/sid/Sports_Outdoors/checkpoint_120000/itemid_to_sid.csv --jsonl /mnt/data/Sports_Outdoors_items.jsonl -m 10 -k 5 --seed 2020 \
-    --output /mnt/graduation_paper/sid/sid效果评测/抽样结果/Sports_Outdoors/10个item的
+
+python /path --csv /mnt/data/sid/Beauty/checkpoint_120000/itemid_to_sid.csv --jsonl /mnt/data/Beauty_items.jsonl -m 10 -k 5 --seed 2020 \
+    --output /path
+
+
+python /path --csv /mnt/data/sid/Sports_Outdoors/checkpoint_120000/itemid_to_sid.csv --jsonl /mnt/data/Sports_Outdoors_items.jsonl -m 10 -k 5 --seed 2020 \
+    --output /path
